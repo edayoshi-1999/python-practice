b3980c782ade381354868c5d3cbd29195d2874f0
edayoshi-1999 <st24012@g.u-fukui.ac.jp>
2025-02-08 13:03:03 +0900

    2/8 13:03 chapter3とjsonファイルの追加

---
+++ chapter3.py
@@ -0,0 +1,52 @@
+# print("Hello")
+
+# x = 11
+# if x > 10:
+#     print("xは10より大きい")
+# elif x == 10:
+#     print("xは10である")
+# else:
+#     print("xは10より小さい")
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     i += 1
+
+# i = 3
+# while i:
+#     print(i)
+#     i -= 1
+
+# for i in range(1,5):
+#     print(i)
+
+# num = 1
+# for i in range(1,10):
+#     num *= 2
+#     print(i, num)
+#     if num > 20:
+#         break
+
+# for i in range(1,10):
+#     if i % 3 == 0 or i % 5 == 0:
+#         continue
+#     print(i)
+
+# for i in range(1,4):
+#     for j in range(1,3):
+#         print(i, j)
+
+# x = 10
+# y = "0"
+# try:
+#     z = x/y
+#     print("計算の後に出力")
+# except ZeroDivisionError as e:
+#     print("ZeroDivisionエラーが起きました。",e)
+# except TypeError as e:
+#     print("TypeError発生", e)
+# else:
+#     print("エラーが発生しませんでした。")
+# finally:
+#     print("例外処理が終了しました。")
